cd5ab0f4064703fa788b76313c6c2a7eed7f8b6d
jackopuff <jackson@carterrealm.com>
2017-06-02 13:06:48 -0600

    Update Game1.cs

--- CoolGuyQuest/Controller/Game1.cs
+++ CoolGuyQuest/Controller/Game1.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace MemeDream
+namespace CoolGuyQuest.Controller
 {
 	/// <summary>
 	/// This is the main type for your game.

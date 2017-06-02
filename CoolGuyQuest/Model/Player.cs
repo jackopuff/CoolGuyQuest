1ab3d7e2e40962d57057422b34793f87266a5d24
jackopuff <jackson@carterrealm.com>
2017-06-02 13:08:35 -0600

    Update Player.cs

--- CoolGuyQuest/Model/Player.cs
+++ CoolGuyQuest/Model/Player.cs
@@ -13,7 +13,7 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace Platformer
+namespace CoolGuyQuest.Model
 {
     /// <summary>
     /// Our fearless adventurer!
